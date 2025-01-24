2d58dcfc167ac36634ce2fb70b636a8995ac0aa2
Ryan Williams <ryan.rpwilliams@gmail.com>
2025-01-24 12:26:28 -0500

    fixed ui with Vite

--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -1,33 +1,14 @@
 import React from "react";
-import { Layout, Menu } from "antd";
-import {
-    LineChartOutlined,
-    BarChartOutlined,
-    TableOutlined,
-    ExperimentOutlined,
-} from "@ant-design/icons";
 import { Link } from "react-router-dom";
-
-const { Sider } = Layout;
 
 const Sidebar = () => {
     return (
-        <Sider collapsible>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1" icon={<LineChartOutlined />}>
-                    <Link to="/">Live Market</Link>
-                </Menu.Item>
-                <Menu.Item key="2" icon={<BarChartOutlined />}>
-                    <Link to="/backtesting">Backtesting</Link>
-                </Menu.Item>
-                <Menu.Item key="3" icon={<TableOutlined />}>
-                    <Link to="/data-table">Data Table</Link>
-                </Menu.Item>
-                <Menu.Item key="4" icon={<ExperimentOutlined />}>
-                    <Link to="/analyses">Analyses</Link>
-                </Menu.Item>
-            </Menu>
-        </Sider>
+        <div style={{ width: "200px", backgroundColor: "#f0f2f5", height: "100vh" }}>
+            <ul>
+                <li><Link to="/">Dashboard</Link></li>
+                <li><Link to="/backtesting">Backtesting</Link></li>
+            </ul>
+        </div>
     );
 };
 
