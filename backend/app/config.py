bc96bad33c11f08586bf043e1c01ca6ad0f2dd98
Ryan Williams <ryan.rpwilliams@gmail.com>
2025-01-27 10:27:45 -0500

    temp

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,6 +2,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://user:password@db:5432/ishara_db"
+    ALPACA_API_KEY: str
+    ALPACA_SECRET_KEY: str
+    ALPACA_BASE_URL: str = "https://paper-api.alpaca.markets"
     REDIS_URL: str = "redis://redis:6379/0"
 
     class Config:
